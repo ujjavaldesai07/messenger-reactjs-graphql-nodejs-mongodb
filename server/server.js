b4dc7b20a83038d1cf87bfaf865ca98c8b0d9d64
ujjaval <ujjavaldesai5891@gmail.com>
2020-08-20 22:45:17 -0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const _ = require('lodash')
+const {GraphQLServer} = require('graphql-yoga')
+
+
+const messages = [];
+
+// graphql schema
+const typeDefs = `
+type Message {
+    id: ID!
+    user: String!
+    content: String!
+}
+
+type Query {
+    messages: [Message!]
+}
+
+type Mutation {
+    postMessage(user: String!, content: String!): ID!
+}
+`;
+
+const resolvers = {
+    Query: {
+        messages: () => messages,
+    },
+    Message: {
+
+    },
+    Mutation: {
+        postMessage: (parent, {user, content}) => {
+            const id = messages.length;
+            messages.push({
+                id,
+                user,
+                content
+            });
+            return id
+        }
+    }
+};
+
+const server = new GraphQLServer({typeDefs, resolvers});
+
+server.start(({port}) => {
+    console.log(`Server on http://localhost:${port}/`);
+});
